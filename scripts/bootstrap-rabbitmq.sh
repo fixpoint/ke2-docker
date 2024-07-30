98b413685a48961c3a1bf7ce15ab3224c1a74bc7
Kenta Hattori <hattori@fixpoint.co.jp>
2024-07-31 08:49:20 +0900

    Fixed: issue #34

--- scripts/bootstrap-rabbitmq.sh
+++ scripts/bootstrap-rabbitmq.sh
@@ -8,5 +8,14 @@ chown -R rabbitmq:rabbitmq /etc/rabbitmq/ssl
 # SSL 認証プラグインを有効化
 rabbitmq-plugins enable rabbitmq_auth_mechanism_ssl
 
+# クラスタ構成時は、再参加できるようにメンバーをクリアしておく
+for cluster_node in $(grep '^cluster_formation.classic_config.nodes' /etc/rabbitmq/conf.d/*.conf | cut -d'=' -f2); do
+    if [[ rabbit@$(hostname) != ${cluster_node} ]]; then
+        if rabbitmqctl -n ${cluster_node} forget_cluster_node rabbit@$(hostname) 2> /dev/null; then
+            break
+        fi
+    fi
+done
+
 # rabbitmq-server を起動
 exec rabbitmq-server "$*"
